ab2f2df64fc6c696892a979ac127f661a01f4715
vxz1datoubo <15322898848@163.com>
2026-08-25 18:52:49 +0800

    Expose Director Feature Compiler through learning retriever package

--- tools/learning_retriever/learning_retriever/__init__.py
+++ tools/learning_retriever/learning_retriever/__init__.py
@@ -1,3 +1,19 @@
+from .feature_compiler import (
+    DirectorFeatures,
+    FeatureCompilationError,
+    compile_director_features,
+    compile_retrieval_task,
+    validate_semantic_dependencies,
+)
 from .retriever import LearningRetriever, RetrievalGateError, validate_index
 
-__all__ = ["LearningRetriever", "RetrievalGateError", "validate_index"]
+__all__ = [
+    "DirectorFeatures",
+    "FeatureCompilationError",
+    "LearningRetriever",
+    "RetrievalGateError",
+    "compile_director_features",
+    "compile_retrieval_task",
+    "validate_index",
+    "validate_semantic_dependencies",
+]
