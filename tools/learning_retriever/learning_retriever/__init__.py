8295f4af51321a941ed262e053997182322d56e6
vxz1datoubo <15322898848@163.com>
2026-08-25 19:18:19 +0800

    Export canonical director learning runtime

--- tools/learning_retriever/learning_retriever/__init__.py
+++ tools/learning_retriever/learning_retriever/__init__.py
@@ -6,14 +6,19 @@ from .feature_compiler import (
     validate_semantic_dependencies,
 )
 from .retriever import LearningRetriever, RetrievalGateError, validate_index
+from .route_resolver import RouteResolutionError, resolve_hard_routes
+from .runtime import DirectorLearningRuntime
 
 __all__ = [
     "DirectorFeatures",
+    "DirectorLearningRuntime",
     "FeatureCompilationError",
     "LearningRetriever",
     "RetrievalGateError",
+    "RouteResolutionError",
     "compile_director_features",
     "compile_retrieval_task",
+    "resolve_hard_routes",
     "validate_index",
     "validate_semantic_dependencies",
 ]
