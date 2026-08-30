8f997445d8448ce26745306f5a721bda9687308d
vxz1datoubo <15322898848@163.com>
2026-08-30 19:49:33 +0800

    Export Active Work Item v2 runtime API

--- tools/learning_retriever/learning_retriever/__init__.py
+++ tools/learning_retriever/learning_retriever/__init__.py
@@ -1,3 +1,15 @@
+from .active_work_item import (
+    ActiveWorkItemResolutionError,
+    WorkItemResolution,
+    apply_constraint_ledger,
+    build_work_item_context_packet,
+    is_continuation_request,
+    load_active_work_item_state,
+    resolve_work_item,
+    validate_output_work_item,
+    validate_state_transition,
+    validate_work_item_context_packet,
+)
 from .cinematic_intent import (
     CinematicIntentContract,
     CinematicIntentContractError,
@@ -18,6 +30,7 @@ from .route_resolver import RouteResolutionError, resolve_hard_routes
 from .runtime import DirectorLearningRuntime
 
 __all__ = [
+    "ActiveWorkItemResolutionError",
     "CinematicIntentContract",
     "CinematicIntentContractError",
     "Diagnostic",
@@ -27,12 +40,21 @@ __all__ = [
     "LearningRetriever",
     "RetrievalGateError",
     "RouteResolutionError",
+    "WorkItemResolution",
+    "apply_constraint_ledger",
+    "build_work_item_context_packet",
     "compile_cinematic_intent_contract",
     "compile_director_features",
     "compile_retrieval_task",
     "evaluate_cinematic_intent",
+    "is_continuation_request",
+    "load_active_work_item_state",
     "resolve_hard_routes",
+    "resolve_work_item",
     "validate_cinematic_intent_contract",
     "validate_index",
+    "validate_output_work_item",
     "validate_semantic_dependencies",
+    "validate_state_transition",
+    "validate_work_item_context_packet",
 ]
